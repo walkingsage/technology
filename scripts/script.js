23108b342eb65d1798cc297b5bb9c32f2e14b5cd
walkingsage <walkingsage@mail.ru>
2020-04-29 17:20:04 +0300

    menu parallax add

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 const  menu=document.getElementById('menu');
 
+window.addEventListener('scroll', function(){
+    if(pageYOffset >= 150){
+        menu.classList.add('menu__paralax');
+    }
+    else{
+        menu.classList.remove('menu__paralax');
+    }
+});
+
